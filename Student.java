5d6364f278b6708bb000991f9f258e30c2cdc9a7
karkera <karkera@karkeras-MacBook-Air.local>
2025-02-01 17:44:44 +0530

    changed operations.java

--- Student.java
+++ Student.java
@@ -1,57 +1,51 @@
-import java.util.*;
 
-public class Operations {
 
-    public Map<Integer, Student> studentMap = new HashMap<>();
-
-    Scanner sc = new Scanner(System.in);
-    
-    Random rand = new Random();
-
-    public void enterstudentdetails(){
-        Student st = new Student();
-
-        int randomNumber = 10000000 + rand.nextInt(90000000);
-            try{
-            System.out.print("ENTER FIRST NAME : ");
-            st.setFirstname(sc.nextLine());
-            System.out.print("ENTER LAST NAME : ");
-            st.setLastname(sc.nextLine());
-            System.out.println("STUDENT ID : " + randomNumber );
-            st.setStudentid(randomNumber);
-            System.out.print("CLASS NO : ");
-            st.setClassno(sc.nextInt());
-            sc.nextLine();
-            System.out.print("SECTION : ");
-            st.setSection(sc.nextLine().toUpperCase().charAt(0));
-            //System.out.println("+++++++++++++++++++++++++++++++++++");
-            studentMap.put(st.getStudentid() , st);
-            st.displayStudent();
-            }catch(Exception e){
-                System.err.println(e);
-                System.out.println(e);
-            }
+public class Student {
+       
+    String firstname;
+    String lastname;
+    int studentid;
+    int classno;
+    char section;
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname.toUpperCase();
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname.toUpperCase();
+    }
+    public int getStudentid() {
+        return studentid;
+    }
+    public void setStudentid(int studentid) {
+        this.studentid = studentid;
+    }
+    public int getClassno() {
+        return classno;
+    }
+    public void setClassno(int classno) {
+        this.classno = classno;
+    }
+    public char getSection() {
+        return section;
+    }
+    public void setSection(char section) {
+        this.section = section;
     }
 
-    public void displayallStudent(){
-        if(studentMap.isEmpty()){
-            System.out.println("No data present");
-        }else{
-            for(Student student : studentMap.values()){
-               student.displayStudent();
-            }
-        }
-    }
-
-    public void displayStudentid(int studentid){
-        Student st = studentMap.get(studentid);
-
-        if(st != null){
-            st.displayStudent();
-        }else{
-            System.out.println("Student ID : " + studentid + " not found.");
-        }
+    public  void displayStudent(){
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("First Name : "+ getFirstname());
+        System.out.println("Last Name : " + getLastname());
+        System.out.println("Student ID : "+ getStudentid());
+        System.out.println("Class : " + getClassno());
+        System.out.println("Section : " + getSection());
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
     }
     
-
 }
